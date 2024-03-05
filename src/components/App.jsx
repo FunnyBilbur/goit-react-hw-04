1db545e69eae584fc1b73f5b663f9f6d06d64493
Dmytrii Tsybuliak <tonistark1997@gmail.com>
2024-03-05 16:50:24 +0300

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,68 +1,27 @@
-import './App.css';
-import { useState, useEffect } from 'react';
-import Options from './Options/Options';
-import Description from './Description/Description';
-import Feedback from './Feedback/Feedback';
-import Notification from './Notification/Notification';
+import { useRef } from 'react';
 
-export default function App() {
-  //useState for values{good, neutral, bad}
-  const [value, setValue] = useState(() => {
-    //get value from localStorage or set default 0, 0, 0
-    const feedbackValues = window.localStorage.getItem('feedback-values');
-    if (feedbackValues !== null) {
-      return JSON.parse(feedbackValues);
-    }
-    return {
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    };
-  });
+const Player = ({ source }) => {
+  const playerRef = useRef();
 
-  //onUpdate value - update localStorage
-  useEffect(() => {
-    window.localStorage.setItem('feedback-values', JSON.stringify(value));
-  }, [value]);
+  const play = () => playerRef.current.play();
 
-  //update feedback depands of feedbackType through setValue from useState
-  const updateFeedback = feedbackType => {
-    setValue({
-      ...value,
-      [feedbackType]: value[feedbackType] + 1,
-    });
-  };
-
-  //reset value through setValue(0,0,0)
-  const resetFeedback = () => {
-    setValue({
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    });
-  };
-  //destructure elements from value
-  const { good, neutral, bad } = value;
-
-  //count totalFeedback
-  const totalFeedback = good + neutral + bad;
+  const pause = () => playerRef.current.pause();
 
   return (
     <div>
-      <Description></Description>
-      <Options
-        onGoodAction={() => updateFeedback('good')}
-        onNeutralAction={() => updateFeedback('neutral')}
-        onBadAction={() => updateFeedback('bad')}
-        onReset={resetFeedback}
-        totalValue={totalFeedback}
-      ></Options>
-
-      {totalFeedback === 0 ? (
-        <Notification></Notification>
-      ) : (
-        <Feedback getValue={value} totalValue={totalFeedback}></Feedback>
-      )}
+      <video ref={playerRef} src={source}>
+        Sorry, your browser does not support embedded videos.
+      </video>
+      <div>
+        <button onClick={play}>Play</button>
+        <button onClick={pause}>Pause</button>
+      </div>
     </div>
   );
-}
+};
+
+const App = () => {
+  return <Player source="<http://media.w3.org/2010/05/sintel/trailer.mp4>" />;
+};
+
+export default App;
